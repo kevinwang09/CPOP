43c7d090f3b2f7e72fc640c215575b6143a1ef81
kevinwang09 <kevin.wang@sydney.edu.au>
2019-05-15 09:51:27 +1000

    modify travis, suggests

--- R/compute_weights.R
+++ R/compute_weights.R
@@ -9,9 +9,9 @@
 #' p = 10
 #' x1 = matrix(rnorm(n * n, mean = 0, sd = 1), nrow = n, ncol = p)
 #' x2 = matrix(rnorm(n * p, mean = 1, sd = 1), nrow = n, ncol = p)
-#' feature_weights(x1, x2)
+#' compute_weights(x1, x2)
 
-feature_weights = function(x1, x2){
+compute_weights = function(x1, x2){
 
   stopifnot({
     identical(ncol(x1), ncol(x2))
---
+++ R/top1.R
@@ -0,0 +1,10 @@
+#' @title Step 1 of the TOP method
+#' @description Step 1 of the TOP method
+#' @param x A data matrix
+#' @return A vector
+#' @export
+#' @examples
+#' top1(1:5)
+top1 = function(x){
+  print(x)
+}
