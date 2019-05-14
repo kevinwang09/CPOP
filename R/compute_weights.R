b4b0b07e28ed89cad3eacf0607e3c76dfb6bf21d
kevinwang09 <kevin.wang@sydney.edu.au>
2019-05-14 18:30:31 +1000

    added penalty function and travis

---
+++ R/compute_weights.R
@@ -0,0 +1,32 @@
+#' @title Compute feature-wise concordance weights
+#' @description Compute feature-wise concordance weights
+#' @param x1 A data matrix
+#' @param x2 A data matrix
+#' @return A vector
+#' @examples
+#' n = 100
+#' p = 10
+#' x1 = matrix(rnorm(n * n, mean = 0, sd = 1), nrow = n, ncol = p)
+#' x2 = matrix(rnorm(n * p, mean = 1, sd = 1), nrow = n, ncol = p)
+#' feature_weights(x1, x2)
+
+feature_weights = function(x1, x2){
+
+  stopifnot({
+    identical(ncol(x1), ncol(x2))
+  })
+
+  mX1 = colMeans(x1)
+  mX2 = colMeans(x2)
+
+  p = ncol(x1)
+  absPenalty = pTransform(abs(mX1 - mX2))
+  return(absPenalty)
+}
+
+pTransform = function(penalty){
+  p = length(penalty)
+  newPenalty1 = ifelse(penalty <= 0, 0, penalty)
+  newPenalty2 = newPenalty1*p/sum(newPenalty1)
+  return(newPenalty2)
+}
--- R/hello.R
+++
@@ -1,6 +0,0 @@
-#' @title Hello
-#' @description Is it me you're looking for?
-#' @export
-hello <- function() {
-  print("Hello, world!")
-}
