5fb3d5a7f06f3d8f9ce9c904afde9dfe88791274
kevinwang09 <kevin.wang@sydney.edu.au>
2019-05-15 11:34:30 +1000

    deleted and reinitialised SSH

---
+++ R/pairwise_col_diff.R
@@ -0,0 +1,38 @@
+#' @title Compare pairwise diff
+#' @description Compare pairwise diff
+#' @param x A data matrix
+#' @return A pairwise difference matrix
+#' @importFrom magrittr set_colnames
+#' @importFrom magrittr %>%
+#' @importFrom purrr map
+#' @importFrom purrr map2
+#' @export
+#' @examples
+#' n = 4
+#' p = 4
+#' x = matrix(rep(1:p, n), nrow = n, ncol = p, byrow = TRUE)
+#' colnames(x) = paste0("X", 1:p)
+#' pairwise_col_diff(x)
+
+pairwise_col_diff = function(x){
+  all_pair = utils::combn(colnames(x), 2) %>% t %>%
+    magrittr::set_colnames(c("Gene1", "Gene2"))
+
+  all_pair_names = paste(all_pair[,"Gene1"], all_pair[,"Gene2"], sep = "--")
+
+  res = form_pairs(all_pair = all_pair,
+                   data = x,
+                   all_pair_names = all_pair_names)
+  return(res)
+}
+
+
+form_pairs = function(all_pair, data, all_pair_names){
+  diff_mat = purrr::map2(
+    .x = all_pair[,"Gene1"],
+    .y = all_pair[,"Gene2"],
+    .f = ~ data[,.x] - data[,.y])
+  names(diff_mat) = all_pair_names
+  res = diff_mat %>% do.call(cbind, .)
+  return(res)
+}
