cb735d7660a6f6a542cf2a0b5304333674627f75
kevinwang09 <kevin.wang@sydney.edu.au>
2019-06-06 11:03:48 +1000

    upd plotting fun

--- R/plot_top_coef.R
+++ R/plot_top_coef.R
@@ -2,6 +2,7 @@
 #' @description Plot top coefficients
 #' @param top_model_result The output of top_model
 #' @param s laso s
+#' @param type "scatter" or "bar"
 #' @import ggplot2
 #' @importFrom tibble tibble
 #' @importFrom tidyr gather
@@ -13,10 +14,11 @@
 #' n = 100
 #' p = 10
 #' x1 = matrix(rnorm(n * p, mean = 0, sd = 1), nrow = n, ncol = p)
-#' x2 = x1 + 0.1
+#' x2 = matrix(rnorm(n * p, mean = 0, sd = 1), nrow = n, ncol = p)
 #' colnames(x1) = colnames(x2) = paste0("X", 1:p)
 #' k = 2
 #' beta = c(rep(1, k), rep(0, p - k))
+#' x2[,1] = x2[,1] + 1
 #' expit = function(x) 1/(1+exp(-x))
 #' y1 = rbinom(n, 1, prob = expit(x1 %*% beta))
 #' y2 = rbinom(n, 1, prob = expit(x2 %*% beta))
@@ -25,11 +27,12 @@
 #' w = compute_weights(z1, z2)
 #' top_model_result = top_model(z1, z2, y1, y2, w = w,
 #' alpha = 1, n_features = 40, s = "lambda.min")
-plot_top_coef = function(top_model_result, s = "lambda.min"){
+#' plot_top_coef(top_model_result, s = "lambda.min")
+plot_top_coef = function(top_model_result, s = "lambda.min", type = "scatter"){
   top_model_result$en1$lambda.min
   top_model_result$en2$lambda.min
-  coef_en1 = get_lasso_coef(top_model_result$en1, s = "lambda.min")
-  coef_en2 = get_lasso_coef(top_model_result$en2, s = "lambda.min")
+  coef_en1 = get_lasso_coef(top_model_result$en1, s = s)
+  coef_en2 = get_lasso_coef(top_model_result$en2, s = s)
 
   stopifnot(identical(rownames(coef_en1),
                       rownames(coef_en2)))
@@ -41,18 +44,24 @@ plot_top_coef = function(top_model_result, s = "lambda.min"){
       coef_name = forcats::fct_reorder(coef_name, coef_en1)
     )
 
-  ggplot(coef_plotdf,
-         aes(x = coef_en1, coef_en2)) +
-    geom_point() +
-    geom_abline(slope = 1, intercept = 0, colour = "red")
+  if(type == "scatter"){
+    g1 = ggplot(coef_plotdf,
+                aes(x = coef_en1, coef_en2)) +
+      geom_point() +
+      geom_abline(slope = 1, intercept = 0, colour = "red")
 
+    return(g1)
+  }
 
-  coef_plotdf_gather = coef_plotdf %>%
-    tidyr::gather(coef_key, coef_value, -coef_name)
+  if(type == "bar"){
+    coef_plotdf_gather = coef_plotdf %>%
+      tidyr::gather(coef_key, coef_value, -coef_name)
 
-  coef_plotdf_gather %>%
-    ggplot(aes(x = coef_name, y = coef_value,
-               fill = coef_key)) +
-    geom_col()
+    g2 = coef_plotdf_gather %>%
+      ggplot(aes(x = coef_name, y = coef_value,
+                 fill = coef_key)) +
+      geom_col()
+    return(g2)
+  }
 
 }
