2d5aaf1f3f7481d9d5e34fe350ddcc7ddc3f374f
Kevin Wang <kevin.wang@sydney.edu.au>
2020-02-08 18:54:32 +0000

    upd behaviour of predict of CPOP

--- R/predict_cpop.R
+++ R/predict_cpop.R
@@ -8,6 +8,9 @@
 #' @importFrom glmnet cv.glmnet
 #' @importFrom glmnet coef.glmnet
 #' @importFrom tibble as_tibble
+#' @importFrom dplyr select
+#' @importFrom dplyr mutate
+#' @importFrom dplyr everything
 #' @return A vector
 #' @export
 #' @examples
@@ -18,10 +21,12 @@
 #' w = compute_weights(z1, z2)
 #' cpop_result = cpop_model(z1, z2, y1, y2, w = w,
 #' alpha = 1, n_features = 40, s = "lambda.min", family = "binomial")
-#' head(predict_cpop(cpop_result, newz = z1, model_number = 1))
-#' head(predict_cpop(cpop_result, newz = z1, model_number = "both", tibble = TRUE))
-#' head(predict_cpop(cpop_result, newz = z1, model_number = "avg", tibble = TRUE))
-predict_cpop = function(cpop_result, newz, s = "lambda.min", model_number = 1L, tibble = FALSE){
+#' rownames(z1) = paste0("sample",1:nrow(z1))
+#' newz = z1[sample(1:5, 10, replace = TRUE),]
+#' head(predict_cpop(cpop_result, newz = newz, model_number = 1))
+#' head(predict_cpop(cpop_result, newz = newz, model_number = "both"))
+#' head(predict_cpop(cpop_result, newz = newz, model_number = "avg"))
+predict_cpop = function(cpop_result, newz, s = "lambda.min", model_number = 1L, tibble = TRUE){
   ## If any discovered featureset is not in newz, then stop
   if(!all(cpop_result$feature %in% colnames(newz))){
     stop("cpop_result feature must be a strict subset of colnames(newz)")
@@ -52,12 +57,15 @@ predict_cpop = function(cpop_result, newz, s = "lambda.min", model_number = 1L, 
     colnames(result) = c("cpop_model1", "cpop_model2", "cpop_model_avg")
   }
 
-  # rownames(result) = rownames(newz)
+
 
   if(tibble){
-    result = tibble::rownames_to_column(data.frame(result), var = "samples")
-    result = tibble::as_tibble(result)
+    tib_result = tibble::as_tibble(data.frame(result))
+    tib_result = dplyr::mutate(tib_result, samples = rownames(result))
+    tib_result = dplyr::select(tib_result, samples, dplyr::everything())
+    return(tib_result)
+  } else {
+    rownames(result) = rownames(newz)
+    return(result)
   }
-
-  return(result)
 }
