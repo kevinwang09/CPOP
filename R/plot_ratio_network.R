91e029e779674b6a6fe93c91c2313fd87ddca438
Kevin Wang <kevin.wang@sydney.edu.au>
2020-05-04 00:14:57 +1000

    redesign the network visualisations

--- R/plot_ratio_network.R
+++ R/plot_ratio_network.R
@@ -1,58 +1,102 @@
 #' @title Plot ratio network
 #' @description  Plot ratio network
-#' @param x The output of top1 or top2 or get_lasso_coef
-#' @param nodesData Logical
-#' @param stat_df stat_df
-#' @importFrom tibble tibble
-#' @importFrom tidyr separate
-#' @importFrom visNetwork visNetwork
+#' @param x A character vector representing edges by separating nodes with "--"
+#' i.e. in the form of "e1--e2"
+#' @param type Type of graphical visualisation, currently only support one of
+#' "visNetwork", "igraph", "ggraph"
 #' @import dplyr
+#' @import ggraph
+#' @importFrom stringr str_detect
+#' @rdname cpop_network
 #' @export
 #' @examples
-#' library(visNetwork)
-#' x = c("X1--X10","X1--X3","X2--X4","X1--X4","X1--X8","X1--X9",
-#' "X10--X2","X2--X5","X2--X7","X2--X6")
-#' plot_ratio_network(x, nodesData = FALSE) %>%
-#' visNetwork::visPhysics(solver = "forceAtlas2Based",
-#' forceAtlas2Based = list(gravitationalConstant = -100))
-#' plot_ratio_network(x, nodesData = TRUE)
-plot_ratio_network = function(x, nodesData = FALSE, stat_df = NULL){
-  if(is.character(x)){
-    edges = tibble::tibble(x) %>%
-      tidyr::separate(col = x,
-                      into = c("from", "to"),
-                      sep = "--")
+#' x = c("X1--X10","X1--X3", "X1--X4","X1--X8","X1--X9","X2--X5","X2--X7","X2--X6", "X8--X10")
+#' plot_lratio_network(x, type = "ggraph")
+#' plot_lratio_network(x, type = "igraph")
+#' plot_lratio_network(x, type = "visNetwork")
 
-    nodes = tibble::tibble(id = unlist(edges)) %>%
-      dplyr::group_by(id) %>%
-      dplyr::summarise(
-        nNeighbours = n(),
-        size = 10*nNeighbours,
-        font.size = 0.5*size) %>%
-      dplyr::mutate(label = id)
-  } else {
-    stopifnot(!is.null(stat_df))
+plot_lratio_network = function(x, type = "ggraph"){
+  assertthat::assert_that(type %in% c("visNetwork", "igraph", "ggraph"),
+                          msg = "Only ggraph, visNetwork and igraph visualisations are supported")
+  assertthat::assert_that(all(stringr::str_detect(x, "--")),
+                          msg = "All edges must use the separator of '--'")
 
-    edges = x %>%
-      tidyr::separate(col = feature_name,
-                      into = c("from", "to"),
-                      sep = "--") %>% as_tibble() %>%
-      dplyr::mutate(
-        color = "gray",
-        value = 10*beta,
-        title = signif(beta, 2))
+  network = lratio_to_network(x)
+  edges = network$edges
+  nodes = network$nodes
 
-    nodes = tibble::tibble(id = c(edges$from, edges$to)) %>%
-      dplyr::group_by(id) %>%
-      dplyr::summarise(nNeighbours = n()) %>%
-      left_join(stat_df, by = "id") %>%
-      dplyr::mutate(label = id)
+  if(type == "visNetwork"){
+    assertthat::assert_that(requireNamespace("visNetwork"),
+                            msg = "You need to install the visNetwork package")
+
+    nodes = nodes %>% dplyr::mutate(
+      label = id,
+      font.size = 20)
+
+    return(visNetwork::visNetwork(nodes, edges))
   }
 
-  if(!nodesData){
-    visNetwork::visNetwork(nodes, edges)
-  } else {
-    return(nodes)
+  if(type == "igraph"){
+    edges_mat = edges %>% as.data.frame %>% as.matrix
+    ig = igraph::graph_from_edgelist(edges_mat, directed = FALSE)
+    plot(ig)
+    return(ig)
   }
+
+  if(type == "ggraph"){
+    assertthat::assert_that(requireNamespace("ggraph"),
+                            msg = "You need to install the ggraph package")
+    edges_mat = edges %>% as.data.frame %>% as.matrix
+    ig = igraph::graph_from_edgelist(edges_mat, directed = FALSE)
+    plot(ig)
+    p = ggraph(ig, layout = "linear", circular = TRUE) +
+      ggraph::geom_edge_arc(aes(
+        start_cap = label_rect(node1.name),
+        end_cap = label_rect(node2.name))) +
+      ggraph::geom_node_text(aes(label = name), size = 6)
+
+    plot(p)
+  }
+
 }
 
+
+#' @title Make a network from lratio
+#' @param x A character vector representing edges by separating nodes with "--"
+#' i.e. in the form of "e1--e2"
+#' @importFrom tibble tibble
+#' @importFrom tidyr separate
+#' @rdname cpop_network
+#' @import dplyr
+#' @export
+lratio_to_network = function(x){
+  assertthat::assert_that(is.character(x))
+
+  edges = tibble::tibble(x) %>%
+    tidyr::separate(col = x,
+                    into = c("from", "to"),
+                    sep = "--")
+
+  nodes = tibble::tibble(id = unlist(edges),
+                         label = id) %>%
+    dplyr::group_by(id) %>%
+    dplyr::tally(name = "n_neighbours") %>%
+    dplyr::ungroup()
+
+  return(lst(edges, nodes))
+}
+
+#' @title Make a network from lratio
+#' @param x A character vector representing edges by separating nodes with "--"
+#' i.e. in the form of "e1--e2"
+#' @importFrom igraph graph_from_edgelist mst
+#' @rdname cpop_network
+mst_lratio = function(x){
+  edges = lratio_to_network(x = x)
+  edges_mat = edges %>% as.data.frame %>% as.matrix
+  ig = igraph::graph_from_edgelist(edges_mat, directed = FALSE)
+  ig_mst = igraph::mst(ig) ## minimum spanning tree removes collinearity
+  ig_mst_edges = get.edgelist(ig_mst)
+  result = paste0(ig_mst_edges[,1], "--", ig_mst_edges[,2])
+  return(result)
+}
