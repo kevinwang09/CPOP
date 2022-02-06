9ff9d1ebf79057e64cf5fec6215135bd877d07f9
Kevin Wang <kevinwang09@users.noreply.github.com>
2022-02-06 21:27:37 +1100

    close #12

--- R/penalties.R
+++ R/penalties.R
@@ -7,8 +7,8 @@
 #' @rdname penalties
 #' @export
 #' @examples
-#' n = 10
-#' p = 10
+#' n = 20
+#' p = 5
 #' x1 = matrix(rnorm(n * p, mean = 0, sd = 1), nrow = n, ncol = p)
 #' x2 = matrix(rnorm(n * p, mean = 1, sd = 1), nrow = n, ncol = p)
 #' colmeans_penalty(x1, x2)
@@ -53,8 +53,8 @@ p_transform = function(penalty){
 #' @rdname penalties
 #' @export
 #' @examples
-#' n = 10
-#' p = 10
+#' n = 20
+#' p = 5
 #' x1 = matrix(rnorm(n * p, mean = 0, sd = 1), nrow = n, ncol = p)
 #' y1 = factor(rbinom(n, 1, prob = 0.5), levels = c("0", "1"))
 #' x2 = matrix(rnorm(n * p, mean = 1, sd = 1), nrow = n, ncol = p)
@@ -68,4 +68,41 @@ tstat_penalty = function(x1, x2, y1, y2){
   t2 = genefilter::colttests(x = x2, fac = y2, tstatOnly = TRUE, na.rm = TRUE)$statistic
   abs_penalty = p_transform(abs(t1 - t2))
   return(abs_penalty)
+}
+
+
+#' @title Compute KS-statistics penalty
+#' @param x1 A data matrix
+#' @param x2 A data matrix
+#' @param statistic Should the KS-test statistic be returned (default) or the p-value should be
+#' returned. Logical.
+#' @return A vector of length matching that of x1 and x2
+#' @importFrom purrr map map_dbl
+#' @rdname penalties
+#' @export
+#' @examples
+#' n = 20
+#' p = 5
+#' x1 = matrix(rnorm(n * p, mean = 0, sd = 1), nrow = n, ncol = p)
+#' y1 = factor(rbinom(n, 1, prob = 0.5), levels = c("0", "1"))
+#' x2 = matrix(rnorm(n * p, mean = 1, sd = 1), nrow = n, ncol = p)
+#' y2 = factor(rbinom(n, 1, prob = 0.5), levels = c("0", "1"))
+#' ks_penalty(x1 = x1, x2 = x2)
+ks_penalty = function(x1, x2, statistic = TRUE){
+
+  assertthat::assert_that(identical(ncol(x1), ncol(x2)))
+
+  p = ncol(x1)
+  ks_result = result = purrr::map(
+    .x = seq_len(p),
+    .f = ~ ks.test(x = x1[,.x], y = x2[,.x], alternative = "two.sided"))
+
+  if(statistic){
+    return(purrr::map_dbl(ks_result, "statistic"))
+  } else {
+    return(purrr::map_dbl(ks_result, "p.value"))
+  }
+
+
+
 }
